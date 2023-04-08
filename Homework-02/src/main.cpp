2efe762727a65522e32999704721b77579e50955
Daniel <user@Ubuntu.myguest.virtualbox.org>
2023-04-08 16:08:56 +0300

    Add HW-02

---
+++ Homework-02/src/main.cpp
@@ -0,0 +1,23 @@
+#include <SDL3/SDL.h>
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    SDL_version compiled = { 0, 0, 0 };
+
+    SDL_version linked = { 0, 0, 0 };
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+
+    std::cout << "compiled: " << static_cast<int>(compiled.major) << '.'
+              << static_cast<int>(compiled.minor) << '.'
+              << static_cast<int>(compiled.patch) << std::endl;
+
+    std::cout << "linked: " << static_cast<int>(compiled.major) << '.'
+              << static_cast<int>(compiled.minor) << '.'
+              << static_cast<int>(compiled.patch) << std::endl;
+
+    return !std::cout.good();
+}
